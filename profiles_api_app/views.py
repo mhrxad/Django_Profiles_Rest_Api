7334866a19689fa0e3a13cf1bba04f2b4ec5b2f5
mehrzad <mhrxadev@gmail.com>
2020-10-28 09:40:56 +0330

    Added viewset to profiles api

--- profiles_api_app/views.py
+++ profiles_api_app/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -28,3 +29,31 @@ class HelloApiView(APIView):
 
     def delete(self, request, pk=None):
         return Response({'message': 'Delete request'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        return Response({'message': 'hello'})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        return Response({'message': 'retrieve'})
+
+    def update(self, request, pk=None):
+        return Response({'message': 'update'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'message': 'partial update'})
+
+    def destroy(self, request, pk=None):
+        return Response({'message': 'destroy'})
