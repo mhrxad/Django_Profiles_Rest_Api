fae6795319b60614d6e73d15038c127ce1fc9bd1
mehrzad <mhrxadev@gmail.com>
2020-10-28 10:36:40 +0330

    Added profiles viewset

--- profiles_api_app/views.py
+++ profiles_api_app/views.py
@@ -2,8 +2,11 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api_app import serializers
+from profiles_api_app import models
+from profiles_api_app import permissions
 
 
 class HelloApiView(APIView):
@@ -57,3 +60,10 @@ class HelloViewSet(viewsets.ViewSet):
 
     def destroy(self, request, pk=None):
         return Response({'message': 'destroy'})
+
+
+class UserProfilrViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
